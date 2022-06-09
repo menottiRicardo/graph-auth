4b79320b13aa3c26400a44d220cf00c7c7dc045c
Ricardo Menotti <ricardomenotti31@gmail.com>
2022-06-08 19:35:01 -0500

    feat: prisma added :star:

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLDateTime } from 'graphql-iso-date';
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       typePaths: ['./**/*.graphql'],
       driver: ApolloDriver,
+      resolvers: { DateTime: GraphQLDateTime },
     }),
 
     UsersModule,
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,7 +1,6 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver('User')
 export class UsersResolver {
